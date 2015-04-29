ce4a987bd3877909b801a68524e6b69bf493a4f7
= <=>
2015-04-29 18:48:13 +0800

    first commit

---
+++ appadmin/config/mis_imgmgr.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['img_level_1_type'] = array(
+		0 => "素描",
+		1 => "色彩",
+		2 => "速写",
+		3 => "设计",
+		4 => "照片"
+);//图片一级分类
