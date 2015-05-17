9064f2570e97dbba3d9d50f1d697a85367ba9ddb
huyuxi <huyuxi@haoche51.com>
2015-05-17 13:34:35 +0800

    批量上传

--- uploadImages/upload.php
+++ uploadImages/upload.php
@@ -1,12 +1,13 @@
 <?php
 $path = '/home/meihua/athena/app/amytian/admin.amytian.com/uploadImages/images/';
 var_dump(upImg($path));
-
+//http://182.92.212.76/upload/tweet_pic
 
 //上传图片
+$imgs = array();
 function upImg($path)
 {
-	$imgs = array();
+	Global $imgs;
 	if(is_dir($path))
 	{
 		if ($dh = opendir($path))
@@ -30,4 +31,21 @@ function upImg($path)
 			closedir($dh);
 		}
 	}
+}
+
+function _curl_post($url, $data){
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+
+	if( ($data = curl_exec($ch)) === false)
+	{
+		echo 'error: ' . curl_error($ch);
+	}
+	curl_close($ch);
+	return $data;
 }
