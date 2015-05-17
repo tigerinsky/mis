50ca2e32ca88ac0f7725dd29383e100b09da01a4
huyuxi <huyuxi@haoche51.com>
2015-05-17 11:48:56 +0800

    批量上传

---
+++ uploadbatch/upload.php
@@ -0,0 +1,31 @@
+<?php
+$path = '/home/meihua/athena/app/amytian/admin.amytian.com/uploadbatch/images/';
+var_dump(upImg($path));
+
+
+//上传图片
+function upImg($path)
+{
+	if(is_dir($path))
+	{
+		if ($dh = opendir($path))
+		{
+			while (($file = readdir($dh)) !== false)
+			{
+				if((is_dir($path."/".$file)) && $file!="." && $file!="..")
+				{
+					echo "<b><font color='red'>文件名：</font></b>",$file,"<br><hr>";
+					upImg($path."/".$file."/");
+				}
+				else
+				{
+					if($file!="." && $file!="..")
+					{
+						echo $file."<br>";
+					}
+				}
+			}
+			closedir($dh);
+		}
+	}
+}
