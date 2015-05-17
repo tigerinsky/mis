28ad2fade98bc9651d6d5d49ee4eacb93d291d84
huyuxi <huyuxi@haoche51.com>
2015-05-17 16:47:58 +0800

    批量上传

--- uploadImages/upload.php
+++ uploadImages/upload.php
@@ -8,7 +8,7 @@ header('content-type:text/html;charset=utf8');
 $ch = curl_init();
 
 //加@符号curl就会把它当成是文件上传处理
-$data = array('img'=>'@'. 'images/img3.jpg;type=image/jpeg;');
+$data = array('img'=>'@'. $path.'img3.jpg;type=image/jpeg;');
 curl_setopt($ch,CURLOPT_URL,$req);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch,CURLOPT_POST,true);
