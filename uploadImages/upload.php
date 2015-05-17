0a400187a7825c288ed744aa7e795af6744d2e74
huyuxi <huyuxi@haoche51.com>
2015-05-17 16:28:04 +0800

    批量上传

--- uploadImages/upload.php
+++ uploadImages/upload.php
@@ -3,22 +3,19 @@ $path = '/home/meihua/athena/app/amytian/admin.amytian.com/uploadImages/images/'
 $req = 'http://182.92.212.76:8081/uploadImages/post.php';
 //$req = 'http://182.92.212.76/upload/tweet_pic';
 
-$file = realpath('/home/meihua/athena/app/amytian/admin.amytian.com/uploadImages/images/img3.jpg'); //要上传的文件
-$fields['f'] = '@'.$file; // 前面加@符表示上传图片
+header('content-type:text/html;charset=utf8');
 
-$ch =curl_init();
+$ch = curl_init();
 
-
+//加@符号curl就会把它当成是文件上传处理
+$data = array('img'=>'@'. '/home/meihua/athena/app/amytian/admin.amytian.com/uploadImages/images/img3.jpg');
 curl_setopt($ch,CURLOPT_URL,$req);
-
-curl_setopt($ch,CURLOPT_POST,true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-
-
-$content = curl_exec($ch);
-
-echo $content;
+curl_setopt($ch,CURLOPT_POST,true);
+curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
+$result = curl_exec($ch);
+curl_close($ch);
+echo json_decode($result);
 
 
 
