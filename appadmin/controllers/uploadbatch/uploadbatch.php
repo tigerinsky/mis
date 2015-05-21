e353e4c8241eb762e080a15ffcafefeb8cde3c02
huyuxi <huyuxi@haoche51.com>
2015-05-22 01:20:38 +0800

    批量上传

--- appadmin/controllers/uploadbatch/uploadbatch.php
+++ appadmin/controllers/uploadbatch/uploadbatch.php
@@ -65,7 +65,6 @@ class uploadbatch extends MY_Controller{
 				$list_data[$key]['s_catalog'] = $this->getClassName($list_data[$key]['s_catalog']);
 			}
 		}
-		var_dump($this->getClassName(119));
 		//一二三级分类
 		$class_1 = array();
 		foreach(self::$cls as $k=>$v)
